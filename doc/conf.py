3f295bbc9b93b64dc9f714ba11c41b282779ed62
bbrument <brument.bcb@gmail.com>
2019-06-20 17:27:13 +0200

    up

--- doc/conf.py
+++ doc/conf.py
@@ -115,7 +115,7 @@ release = "0.0.1"
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = ['build', 'dist']
+exclude_patterns = ['build', 'dist', '**.ipynb_checkpoints']
 
 # The reST default role (used for this markup: `text`) to use for all
 # documents.
