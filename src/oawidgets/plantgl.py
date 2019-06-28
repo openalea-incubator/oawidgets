a88da1fb167e5f1a79142bd52b97f2835bd4f864
bbrument <brument.bcb@gmail.com>
2019-06-28 11:21:36 +0200

    up

--- src/oawidgets/plantgl.py
+++ src/oawidgets/plantgl.py
@@ -1,4 +1,5 @@
 from openalea.plantgl.all import *
+import matplotlib
 import numpy as np
 import k3d
 
@@ -16,7 +17,7 @@ def tomesh(geometry, d=None):
 def scene2mesh(scene):
     """Return a mesh from a scene"""
     d = Tesselator()
-    indices, vertices, colors, attributes=[], [], [], []
+    indices, vertices, colors, attribute=[], [], [], []
     colordict={}
     count=-1
     offset=0
@@ -29,18 +30,19 @@ def scene2mesh(scene):
         if not(color in colordict):
             count+=1
             colordict[color]=count
-        attributes.append(colordict[color])
+        attribute.append(colordict[color])
         offset += len(pts)
         indices.extend(idl.tolist())
     colors=np.array(colordict.keys())/255.
-    color_map=zip(list(np.array(colordict.values())/float(max(colordict.values()))), colors[:,0], colors[:,1], colors[:,2])
-    attributes = list(np.array(attributes)/float(max(attributes)))
-    print attributes
-    print color_map
-    if len(color_map) == 1:
-    	mesh = k3d.mesh(vertices=vertices, indices=indices, color=rgb2hex(colordict.keys()))
+    print colors
+    if len(colors) == 1:
+   	mesh = k3d.mesh(vertices=vertices, indices=indices, color=matplotlib.colors.to_hex(colors[0]))
     else:
-    	mesh = k3d.mesh(vertices=vertices, indices=indices, attributes=attributes, color_map=color_map)
+    	color_map = zip(list(np.array(colordict.values())/float(max(colordict.values()))), colors[:,0], colors[:,1], colors[:,2])
+    	attribute = list(np.array(attribute)/float(max(attribute)))
+    	#print attribute
+    	#print color_map
+    	mesh = k3d.mesh(vertices=vertices, indices=indices, attribute=attribute, color_map=color_map)
     return mesh
 
 
