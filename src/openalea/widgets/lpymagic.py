87f24e956be4c124ed67809072a8edad7a809bfd
baugetfa <fabrice.bauget@cirad.fr>
2025-10-06 13:39:50 +0200

    refactoring namespace

--- src/openalea/widgets/lpymagic.py
+++ src/openalea/widgets/lpymagic.py
@@ -52,7 +52,7 @@ from IPython.utils.py3compat import unicode_to_str
 from openalea.mtg import MTG
 from openalea.mtg.io import mtg2lpy, lpy2mtg
 
-from oawidgets import plantgl
+from openalea.widgets import plantgl
 
 _mimetypes = {'png' : 'image/png',
              'svg' : 'image/svg+xml',
